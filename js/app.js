df78f28f2fc4c06362da137bd0cddde41670cda1
Tony Termine <termine.tony@gmail.com>
2014-04-02 09:31:49 -0400

    First Commit
    Setup HTML,  CSS, and JS.  Need to refine look of the text and
    presentation as well as clean up animation.  Still getting a double
    when the x key is pressed and held and mouse over Ryu.

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+  $('.ryu').mouseenter(function() {
+    $('.ryu-still').hide();
+    $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    playHadouken();
+    $('.ryu-ready').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').finish().show().animate({'left': '1200px'}, 500,function() {
+        $(this).hide();
+        $(this).css('left', '390px');
+      });
+  })
+  .mouseup(function() {
+    $('.ryu-throwing').hide();
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+  });
+  $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-still').hide();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').show();
+    }
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-cool').hide();
+      $('.ryu-still').show();
+    }
+  });
+});
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
